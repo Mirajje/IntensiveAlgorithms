0d70e5aa7a93a26792bb72c0e90938078b056310
Mirajje <96082164+Mirajje@users.noreply.github.com>
2023-08-14 20:28:16 +0300

    Update depth_first_tree_traversal.hpp

--- DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
+++ DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
@@ -1,19 +1,57 @@
-#pragma once
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 
-#include <cstddef>
-#include <vector>
-
-// https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
-
-// Definition for a binary tree node.
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
 
 class Solution {
 public:
-  std::vector<int> distanceK(TreeNode *root, TreeNode *target, int k) {}
+
+    void create_graph(TreeNode* cur_node, std::unordered_map<int, std::unordered_set<int>>& graph)
+    {
+        if (cur_node->left)
+        {
+            graph[cur_node->val].insert(cur_node->left->val);
+            graph[cur_node->left->val].insert(cur_node->val);
+            create_graph(cur_node->left, graph);
+        }
+
+        if (cur_node->right)
+        {
+            graph[cur_node->val].insert(cur_node->right->val);
+            graph[cur_node->right->val].insert(cur_node->val);
+            create_graph(cur_node->right, graph);
+        }
+    }
+
+    void solve(int cur_elem, std::unordered_map<int, std::unordered_set<int>>& graph, std::vector<bool>& visited, int cur_distance, int k, std::vector<int>& result)
+    {
+        if (cur_distance == k)
+            result.push_back(cur_elem);
+
+        for (auto& neighbour : graph[cur_elem])
+            if (!visited[neighbour])
+            {
+                visited[neighbour] = true;
+                solve(neighbour, graph, visited, cur_distance + 1, k, result);
+            }
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        std::unordered_map<int, std::unordered_set<int>> graph;
+        create_graph(root, graph);
+
+        std::vector<bool> visited(501);
+        visited[target->val] = true;
+        std::vector<int> result;
+
+        solve(target->val, graph, visited, 0, k, result);
+
+        return result;
+    }
 };

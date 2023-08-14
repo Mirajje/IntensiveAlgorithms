591cf56d38a16e6642e923cb0a53838d294da5cb
Mirajje <96082164+Mirajje@users.noreply.github.com>
2023-08-14 18:49:13 +0300

    Update selection_sort.hpp

--- SelectionSort/selection_sort.hpp
+++ SelectionSort/selection_sort.hpp
@@ -1,9 +1,25 @@
-#pragma once
-
-#include <vector>
-
-// https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+    vector<int> sortArray(vector<int>& nums) 
+    {
+        int index = 0;
+
+        while (index < nums.size())
+        {
+            int elem = index;
+            int min = index;
+            while (elem < nums.size())
+            {
+                if (nums[elem] < nums[min])
+                    min = elem;
+
+                ++elem;
+            }
+
+            std::swap(nums[index], nums[min]);
+            ++index;
+        }
+
+        return nums;
+    }
 };

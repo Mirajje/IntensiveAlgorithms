c8daa836157ecedf3e8b79abcb1a7c9f0c12ac17
Mirajje <96082164+Mirajje@users.noreply.github.com>
2023-08-14 18:18:29 +0300

    Update bubble_sort.hpp

--- BubbleSort/bubble_sort.hpp
+++ BubbleSort/bubble_sort.hpp
@@ -5,5 +5,13 @@
 // https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+    vector<int> sortArray(vector<int>& nums) 
+    {
+        for (int i = 0; i < nums.size(); ++i)
+            for (int j = i; j < nums.size(); ++j)
+                if (nums[j] < nums[i])
+                    std::swap(nums[i], nums[j]);
+
+        return nums;
+    }
 };

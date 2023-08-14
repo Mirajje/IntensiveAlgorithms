c1d2ef145b74881f054865a5654681166aaa2aed
Mirajje <96082164+Mirajje@users.noreply.github.com>
2023-08-14 21:17:57 +0300

    Update brackets.hpp

--- Brackets/brackets.hpp
+++ Brackets/brackets.hpp
@@ -1,9 +1,21 @@
-#pragma once
-
-#include <string>
-
-// https://leetcode.com/problems/valid-parentheses/
 class Solution {
 public:
-  bool isValid(std::string s) {}
+    bool isValid(string s) 
+    {
+        unordered_map<char, char> map = 
+        {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+        
+        stack<char> stack;
+        for (auto el : s)
+            if (!stack.empty() && stack.top() == map[el])
+                stack.pop();
+            else
+                stack.push(el);
+        
+        return (stack.empty());
+    }
 };

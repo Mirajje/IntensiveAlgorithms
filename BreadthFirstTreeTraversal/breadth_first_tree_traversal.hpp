2fa1319735a465c0cb8de2bc2ca6cb4598cb3248
Mirajje <96082164+Mirajje@users.noreply.github.com>
2023-08-14 21:11:24 +0300

    Update breadth_first_tree_traversal.hpp

--- BreadthFirstTreeTraversal/breadth_first_tree_traversal.hpp
+++ BreadthFirstTreeTraversal/breadth_first_tree_traversal.hpp
@@ -1,21 +1,43 @@
-#pragma once
-
-#include <vector>
-
-// https://leetcode.com/problems/binary-tree-level-order-traversal/
-
-// Definition for a binary tree node.
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
-      : val(x), left(left), right(right) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-  std::vector<std::vector<int>> levelOrder(TreeNode *root) {}
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (!root)
+            return std::vector<std::vector<int>>();
+
+        std::vector<std::vector<int>> result;
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+        int level = 0;
+        while (!queue.empty())
+        {
+            result.push_back(std::vector<int>());
+            int amount = queue.size();
+            for (int i = 0; i < amount; ++i)
+            {
+                auto elem = queue.front();
+                queue.pop();
+                result[level].push_back(elem->val);
+                if (elem->left)
+                    queue.push(elem->left);
+
+                if (elem->right)
+                    queue.push(elem->right);
+            }
+
+            ++level;
+        }
+
+        return result;
+    }
 };

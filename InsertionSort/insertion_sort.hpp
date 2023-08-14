d2f074e8995a4bb1de7ec0579d86eaccdfe63ec8
Mirajje <96082164+Mirajje@users.noreply.github.com>
2023-08-14 18:42:54 +0300

    Update insertion_sort.hpp

--- InsertionSort/insertion_sort.hpp
+++ InsertionSort/insertion_sort.hpp
@@ -1,17 +1,38 @@
-#pragma once
-
-// https://leetcode.com/problems/insertion-sort-list/
-
-// Definition for singly-linked list.
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-  ListNode *insertionSortList(ListNode *head) {}
+    ListNode* insertionSortList(ListNode* head) {
+        auto pos = head;
+
+        while (pos->next)
+        {
+            auto cur = pos;
+            int min = pos->val;
+            auto min_el = pos;
+            
+            while (cur)
+            {
+                if (cur->val < min)
+                {
+                    min = cur->val;
+                    min_el = cur;
+                }
+                cur = cur->next;
+            }
+
+            std::swap(min_el->val, pos->val);
+            pos = pos->next;
+        }
+
+        return head;
+    }
 };
